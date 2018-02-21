0b822de08823ca07e9e5148d5945f5ce0ffed921
Alessandro DI BELLA (contractor) <Alessandro.DIBELLA@amadeus.com>
2018-02-21 11:58:27 +0100

    first commit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +17,21 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView mTVAlsoKnown;
+    private TextView mTVIngredients;
+    private TextView mTVPlaceOfOrigin;
+    private TextView mTVDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mTVAlsoKnown = findViewById(R.id.also_known_tv);
+        mTVIngredients = findViewById(R.id.ingredients_tv);
+        mTVPlaceOfOrigin = findViewById(R.id.origin_tv);
+        mTVDescription = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +54,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +67,10 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        mTVAlsoKnown.setText(TextUtils.join(", ",sandwich.getAlsoKnownAs()));
+        mTVIngredients.setText(TextUtils.join(", ", sandwich.getIngredients()));
+        mTVPlaceOfOrigin.setText(sandwich.getPlaceOfOrigin());
+        mTVDescription.setText(sandwich.getDescription());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,40 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject name = jsonObject.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            ArrayList<String> alsoKnownAsList = jsonArrayToList(alsoKnownAsArray);
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+            JSONArray ingredientsArray = jsonObject.getJSONArray("ingredients");
+            ArrayList<String> ingredientsList = jsonArrayToList(ingredientsArray);
+            return new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    private static ArrayList<String> jsonArrayToList(JSONArray jsonArray) throws JSONException {
+        ArrayList<String> arrayList = new ArrayList<>();
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                arrayList.add(jsonArray.getString(i));
+            }
+        }
+        return arrayList;
     }
 }
